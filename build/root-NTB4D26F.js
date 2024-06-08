c9ba3b48237ce39c41c4b6cd3e1f4a4137ea74ae
KOMANG ANGGADA SUGIARTA <k.anggada@yahoo.co.id>
2024-06-08 21:30:42 +0700

    Updates

---
+++ build/root-NTB4D26F.js
@@ -0,0 +1,15 @@
+import{b as d,d as e,e as o,f as r,g as i,i as a,k as p,l as s,m as l,n as m,q as w,s as g,t as h}from"/build/_shared/chunk-J43WEJVG.js";e();o();r();a();i();e();o();r();a();i();e();o();r();a();i();var k=window.__remixManifest,P=typeof __INJECT_CSS_BUNDLE_HREF__=="string"?__INJECT_CSS_BUNDLE_HREF__:void 0,n=P||k.cssBundleHref;var y="/build/_assets/tailwind-MQRWFA6J.css";var v="/build/_assets/react-photo-view-BLMYC2W4.css";var t=d(h()),z=()=>[...n?[{rel:"stylesheet",href:n}]:[],{rel:"stylesheet",href:y},{rel:"stylesheet",href:v},{rel:"preconnect",href:"https://fonts.googleapis.com"},{rel:"preconnect",href:"https://fonts.googleapis.comhttps://fonts.gstatic.com"},{rel:"stylesheet",href:"https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"},{rel:"stylesheet",href:"https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"}];function _(){return(0,t.jsxs)("html",{lang:"id",children:[(0,t.jsxs)("head",{children:[(0,t.jsx)("meta",{charSet:"utf-8"}),(0,t.jsx)("meta",{name:"viewport",content:"width=device-width,initial-scale=1"}),(0,t.jsx)(l,{}),(0,t.jsx)(s,{})]}),(0,t.jsxs)("body",{suppressHydrationWarning:!0,children:[(0,t.jsx)(p,{}),(0,t.jsx)(g,{}),(0,t.jsx)(m,{}),(0,t.jsx)(w,{})]})]})}export{_ as default,z as links};
+/*! Bundled license information:
+
+@remix-run/css-bundle/dist/esm/browser.js:
+  (**
+   * @remix-run/css-bundle v1.16.0
+   *
+   * Copyright (c) Remix Software Inc.
+   *
+   * This source code is licensed under the MIT license found in the
+   * LICENSE.md file in the root directory of this source tree.
+   *
+   * @license MIT
+   *)
+*/
